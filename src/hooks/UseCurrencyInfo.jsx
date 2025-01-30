64e1f18c83e4441f4f2ef0557fee7b1ab9464ac9
Hrishabh Thakur <hrishabhthakur2@gmail.com>
2025-01-30 12:15:37 +0530

    first commit

---
+++ src/hooks/UseCurrencyInfo.jsx
@@ -0,0 +1,17 @@
+import { useEffect,  useState, useId} from "react";
+
+function UseCurrencyInfo (currency){
+    const [data, setData] = useState({})
+    useEffect(()=>{
+        fetch(`https://api.exchangerate-api.com/v4/latest/{currency}
+`)
+        .then((res) => res.json() )
+        .then((res) => setData(res[currency]))
+        console.log(data);
+    }, [currency])
+    console.log(data)
+    return data
+   
+}
+
+export default UseCurrencyInfo
